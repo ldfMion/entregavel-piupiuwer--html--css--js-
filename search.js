6cb1b790eb093237f6da61f75d0f16a3a688822e
Lorenzo Mion <lorenzodfmion@gmail.com>
2022-05-21 00:08:53 -0300

    comeco do search (filtrar o array sem nada do UI)

---
+++ search.js
@@ -0,0 +1,25 @@
+console.log('search.js')
+
+const searchFieldTextInput = document.getElementById('search-field-text');
+console.log(searchFieldTextInput)
+
+
+fetch("https://arcane-sierra-77337.herokuapp.com/data")
+    .then((response) => response.json()).then(pius => {
+        const users = pius.map(piu => piu.user);
+        // console.log(users)
+        // const uniqueUsers = Array.from(new Set(users));
+        // console.log(uniqueUsers)
+        const uniqueUsers = Array.from(new Set(users.map(user => user.id)))
+        .map(id => {
+          return users.find(user => user.id === id)
+        })
+        console.log(uniqueUsers)
+        searchFieldTextInput.addEventListener('input', (e) => {
+            console.log('is changing')
+            const searchedText = e.target.value;
+            console.log(searchedText)
+            const filteredUsers = uniqueUsers.filter(user => user.username.toLowerCase().includes(e.target.value));
+            console.log(filteredUsers)
+        })
+    })
